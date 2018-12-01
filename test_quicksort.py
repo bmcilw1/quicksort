407d400bf63a2338cb09aff28ddd45faad0fe468
Brian McIlwain <brianwmcilwain@gmail.com>
2018-11-30 22:41:50 -0800

    Added requirements.txt

--- test_quicksort.py
+++ test_quicksort.py
@@ -1,10 +1,15 @@
 #!/usr/local/bin/python3
 
 from hypothesis import given
+from mock import Mock, patch
 import hypothesis.strategies as st
 import quicksort as q
 
 @given(arr=st.lists(st.floats(allow_nan=False, allow_infinity=False) | st.integers()))
 def test_quicksort_sorts(arr):
     arr_sorted = q.quicksort(arr)
+    assert all(arr_sorted[i] <= arr_sorted[i+1] for i in range(len(arr)-1))
+
+def test_main_calls_quicksort(arr):
+    arr_sorted = q.main()
     assert all(arr_sorted[i] <= arr_sorted[i+1] for i in range(len(arr)-1))
