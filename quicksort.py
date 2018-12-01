1cf1b36a474587c4433c0ab8edd64c2726dbcc91
Brian McIlwain <brianwmcilwain@gmail.com>
2018-11-30 20:22:53 -0800

    initial commit

---
+++ quicksort.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python3
+
+import argparse
+
+def quicksort(arr)
+    return arr
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Sort an array of integers using quicksort.')
+    parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                        help='an integer to sort')
+    quicksort(arg)
