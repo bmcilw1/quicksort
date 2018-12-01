3b25be1e2b24066bd1d6c87919c489b188f23671
Brian McIlwain <brianwmcilwain@gmail.com>
2018-11-30 22:08:54 -0800

    Updates test space

--- test_quicksort.py
+++ test_quicksort.py
@@ -4,7 +4,7 @@ from hypothesis import given
 import hypothesis.strategies as st
 import quicksort as q
 
-@given(arr=st.lists(st.integers()))
+@given(arr=st.lists(st.floats(allow_nan=False, allow_infinity=False) | st.integers()))
 def test_quicksort_sorts(arr):
     arr_sorted = q.quicksort(arr)
     assert all(arr_sorted[i] <= arr_sorted[i+1] for i in range(len(arr)-1))
