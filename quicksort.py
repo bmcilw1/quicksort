725fce1bed403516fa40273857b0aa68086c4915
Brian McIlwain <brianwmcilwain@gmail.com>
2018-11-30 20:35:25 -0800

    Added sort functionality

--- quicksort.py
+++ quicksort.py
@@ -3,12 +3,13 @@
 import argparse
 
 def quicksort(arr):
+    arr.sort()
     return arr
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Sort an array of integers using quicksort.')
     parser.add_argument('integers', metavar='Numbers', type=int, nargs='+',
-                        help='a list of integers to sort')
+                        help='The list of integers to sort')
 
     args = parser.parse_args()
     print(quicksort(args.integers))
