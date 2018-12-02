4cc6389684c786bbc896deb85ba6062c0bf0d143
Brian McIlwain <brianwmcilwain@gmail.com>
2018-12-01 23:27:25 -0800

    Cleaned up code

--- quicksort.py
+++ quicksort.py
@@ -10,31 +10,25 @@ def partition(arr, low, high):
     high -= 1
 
     while (low <= high):
-        if arr[low] < arr[pivot]:
+        if arr[low] <= arr[pivot]:
             low += 1
         elif arr[high] > arr[pivot]:
             high -= 1
         else:
             arr[low], arr[high] = arr[high], arr[low]
             low += 1
-    
+            
     arr[pivot], arr[low] = arr[low], arr[pivot]
-    pivot = low
-
-    return pivot
+    return low
 
 def quicksort(arr, low, high):
-    if (low > high):
-        return
-    
-    pivot = partition(arr, low, high)
-    quicksort(arr, low, pivot-1)
-    quicksort(arr, pivot+1, high)
-    return
+    if (low < high):
+        pivot = partition(arr, low, high)
+        quicksort(arr, low, pivot-1)
+        quicksort(arr, pivot+1, high)
 
 def sort(arr):
     quicksort(arr, 0, len(arr)-1)
-
     return arr
 
 def main():
