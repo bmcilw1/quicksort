2b43f06e43ad489eb8cb33ccde94c54cd08bf85b
Brian McIlwain <brianwmcilwain@gmail.com>
2018-12-01 16:19:39 -0800

    Wired up mock

--- test_quicksort.py
+++ test_quicksort.py
@@ -1,17 +1,17 @@
 #!/usr/local/bin/python3
 
 from hypothesis import given
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, Mock, patch
 import hypothesis.strategies as st
 import quicksort as q
+import argparse
 
 @given(arr=st.lists(st.floats(allow_nan=False, allow_infinity=False) | st.integers()))
 def test_quicksort_sorts(arr):
     arr_sorted = q.quicksort(arr)
     assert all(arr_sorted[i] <= arr_sorted[i+1] for i in range(len(arr)-1))
 
-'''
-def test_main_calls_quicksort():
-    arr_sorted = q.main()
-    assert all(arr_sorted[i] <= arr_sorted[i+1] for i in range(len(arr)-1))
-'''
+@patch('quicksort.argparse')
+def test_main_calls_quicksort(argparseMock):
+    q.main()
+    assert argparseMock.ArgumentParser.called_once_with(description='Sort an array of numbers using quicksort.')
